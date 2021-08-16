cb355dcf7f5714b99fe835a375799cf0ac7c9981
e.nazarchuk <e.nazarchuk@gmail.com>
2021-08-16 20:03:00 +0300

    done

--- app/src/main/java/com/evgenynaz/sportnote/bmi/BMIActivity.kt
+++ app/src/main/java/com/evgenynaz/sportnote/bmi/BMIActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.evgenynaz.sportnote.R
+import com.evgenynaz.sportnote.databinding.ActivityBmiBinding
 import kotlinx.android.synthetic.main.activity_bmi.*
 import kotlinx.android.synthetic.main.activity_bmi.toolbar_bmi_activity
 import kotlinx.android.synthetic.main.activity_note_details.*
@@ -13,85 +14,85 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityBmiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmi)
-
+        binding = ActivityBmiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setSupportActionBar(toolbar_bmi_activity)
         val actionBar = supportActionBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
         // вернуться назад
-        toolbar_bmi_activity.setNavigationOnClickListener {
+        binding.toolbarBmiActivity.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        calculate_units_btn.setOnClickListener {
-                if (validateMetricUnits()) {
-                    val heightValue: Float =
-                        metric_unit_height_et.text.toString().toFloat() / 100 // convert to meters
-                    val weightValue: Float = metric_unit_weight_et.text.toString().toFloat()
+        binding.calculateUnitsBtn.setOnClickListener {
+            if (validateMetricUnits()) {
+                val heightValue: Float =
+                    binding.metricUnitHeightEt.text.toString().toFloat() / 100
+                val weightValue: Float = binding.metricUnitWeightEt.text.toString().toFloat()
 
-                    val bmi = weightValue / (heightValue * heightValue)
-                    displayBMIResult(bmi)
-                } else {
-                    Toast.makeText(this, "Пожалуйста введите корректные данные", Toast.LENGTH_SHORT)
-                        .show()
-                }
+                val bmi = weightValue / (heightValue * heightValue)
+                displayBMIResult(bmi)
+            } else {
+                Toast.makeText(this, "Пожалуйста введите корректные данные", Toast.LENGTH_SHORT)
+                    .show()
             }
-
         }
+
+    }
 
     private fun displayBMIResult(bmi: Float) {
         val bmiLabel: String
         val bmiDescription: String
 
         if (bmi.compareTo(15f) <= 0) {
-            bmiLabel = "Very severely underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+            bmiLabel = "Очень сильно пониженный вес"
+            bmiDescription = "Ой! Вам действительно нужно лучше заботиться о себе! Ешь больше!"
         } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0) {
-            bmiLabel = "Severely underweight"
-            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+            bmiLabel = "Сильно недостаточный вес"
+            bmiDescription = "Упс! Тебе действительно нужно лучше заботиться о себе! Ешь больше!"
         } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0) {
-            bmiLabel = "Underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+            bmiLabel = "Недостаточный вес"
+            bmiDescription = "Ой! Вам действительно нужно лучше заботиться о себе! Ешь больше!"
         } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0) {
-            bmiLabel = "Normal"
-            bmiDescription = "Congratulations! You are in a good shape!"
+            bmiLabel = "Нормальный"
+            bmiDescription = "Поздравляю! Вы в хорошей форме!"
         } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(
                 bmi,
                 30f
             ) <= 0
         ) {
-            bmiLabel = "Overweight"
-            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+            bmiLabel = "Избыточный вес"
+            bmiDescription = "Ой! Вам действительно нужно позаботиться о себе! Может быть, тренировка!"
         } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0) {
-            bmiLabel = "Obese Class | (Moderately obese)"
-            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+            bmiLabel = "Класс ожирения | (Умеренно ожирение)"
+            bmiDescription = "Ой! Вам действительно нужно позаботиться о себе! Может быть, тренировка!"
         } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0) {
-            bmiLabel = "Obese Class || (Severely obese)"
-            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+            bmiLabel = "Класс ожирения || (Сильно ожирение)"
+            bmiDescription = "Вы в очень опасном состоянии! Действовать сейчас!"
         } else {
-            bmiLabel = "Obese Class ||| (Very Severely obese)"
-            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+            bmiLabel = "Класс ожирения ||| (Очень сильно ожирение)"
+            bmiDescription = "Вы в очень опасном состоянии!"
         }
 
-        display_bmi_result_ll.visibility = View.VISIBLE
-        your_bmi_tv.visibility = View.VISIBLE
-        bmi_result_tv.visibility = View.VISIBLE
-        bmi_type_tv.visibility = View.VISIBLE
-        bmi_description_tv.visibility = View.VISIBLE
+        binding.displayBmiResultLl.visibility = View.VISIBLE
+        binding.yourBmiTv.visibility = View.VISIBLE
+        binding.bmiResultTv.visibility = View.VISIBLE
+        binding.bmiTypeTv.visibility = View.VISIBLE
+        binding.bmiDescriptionTv.visibility = View.VISIBLE
 
 
         //округление данных до 2 значений
         val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
 
         //set updated text
-        bmi_result_tv.text = bmiValue
-        bmi_type_tv.text = bmiLabel
-        bmi_description_tv.text = bmiDescription
+        binding.bmiResultTv.text = bmiValue
+        binding.bmiTypeTv.text = bmiLabel
+        binding.bmiDescriptionTv.text = bmiDescription
     }
 
     private fun validateMetricUnits(): Boolean {
