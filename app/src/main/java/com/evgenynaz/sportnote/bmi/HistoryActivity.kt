c852fabd628bcc10138b550963077c6090d67bd6
e.nazarchuk <e.nazarchuk@gmail.com>
2021-08-16 20:48:27 +0300

    done

--- app/src/main/java/com/evgenynaz/sportnote/bmi/HistoryActivity.kt
+++ app/src/main/java/com/evgenynaz/sportnote/bmi/HistoryActivity.kt
@@ -3,15 +3,15 @@ package com.evgenynaz.sportnote.bmi
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
-import com.evgenynaz.sportnote.R
-import com.evgenynaz.sportnote.databinding.ActivityBmiBinding
-import kotlinx.android.synthetic.main.acticity_history_bmi.*
+
+import com.evgenynaz.sportnote.databinding.ActivityHistoryBmiBinding
+import kotlinx.android.synthetic.main.activity_history_bmi.*
 
 class HistoryActivity:AppCompatActivity() {
-    private lateinit var binding: ActivityBmiBinding
+    private lateinit var binding: ActivityHistoryBmiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityBmiBinding.inflate(layoutInflater)
+        binding = ActivityHistoryBmiBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(toolbar_history_activity)
